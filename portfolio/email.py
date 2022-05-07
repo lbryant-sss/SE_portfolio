aa97e4cfc797ad9ba2cc9dc03ccc159dae223b2e
Just Brian <61902485+lbryant-sss@users.noreply.github.com>
2022-05-07 18:10:45 +0300

    Update email.py

--- portfolio/email.py
+++ portfolio/email.py
@@ -1,5 +1,1 @@
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'brianmungai350@gmail.com'
-EMAIL_HOST_PASSWORD = 'tfyxhczaspkfhnzo'#Enter my third party app pass
-EMAIL_USE_TLS = True
+
